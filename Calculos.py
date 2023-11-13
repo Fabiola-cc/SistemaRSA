93dfffbc395ebce2af21674e4be2c028787b80b4
Fabiola Contreras <fcontrerascol@gmail.com>
2023-11-13 09:37:27 -0600

    adición de docs

---
+++ Calculos.py
@@ -0,0 +1,14 @@
+'''
+Proyecto Final 
+Matemática Discreta
+Sistema RSA
+
+Fabiola Contreras 22787
+María José Villafuerte 22129
+'''
+
+def encriptado():
+    return 0
+
+def desencriptado():
+    return 0
---
+++ Main.py
@@ -0,0 +1,11 @@
+'''
+Proyecto Final 
+Matemática Discreta
+Sistema RSA
+
+Fabiola Contreras 22787
+María José Villafuerte 22129
+'''
+
+def main():
+    return 0
